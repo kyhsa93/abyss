ab52de60f9cc85d9fc85177a5877e43edb8545d8
Younghoon <kyhsa93@naver.com>
2026-06-20 21:16:32 +0900

    feat: add clickable player movement prototype

--- src/game/scenes/DungeonScene.ts
+++ src/game/scenes/DungeonScene.ts
@@ -1,15 +1,36 @@
 import Phaser from 'phaser';
 
 export class DungeonScene extends Phaser.Scene {
+  private player!: Phaser.GameObjects.Arc;
+  private targetX = 400;
+  private targetY = 300;
+
   constructor() {
     super('DungeonScene');
   }
 
   create() {
-    this.add.text(100, 100, 'Abyss Prototype');
+    this.add.text(20, 20, 'Abyss Prototype');
+
+    this.player = this.add.circle(400, 300, 16, 0x00ff00);
 
     this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
-      console.log('move target', pointer.worldX, pointer.worldY);
+      this.targetX = pointer.worldX;
+      this.targetY = pointer.worldY;
     });
+  }
+
+  update() {
+    const speed = 3;
+
+    const dx = this.targetX - this.player.x;
+    const dy = this.targetY - this.player.y;
+
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > 2) {
+      this.player.x += (dx / distance) * speed;
+      this.player.y += (dy / distance) * speed;
+    }
   }
 }
